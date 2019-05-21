e8695ce297c6b863167b20f4483d5cb5fa5babf9
Tadanori TERUYA <tadanori.teruya@gmail.com>
2019-05-21 18:40:14 +0900

    refactoring

--- tools/parse_ntl.py
+++ tools/parse_ntl.py
@@ -36,17 +36,17 @@ def __cstor_matrix(data): # {{{
 def parse_vector(text):
     data = vector.parseString(text).asList()
     return __cstor_vector(data)
-def load_vector(filename):
+def load_vector_file(filename):
     with open(filename) as fd:
         text = fd.read()
     return parse_vector(text)
 def parse_matrix(text):
     data = matrix.parseString(text).asList()
     return __cstor_matrix(data)
-def load_matrix(filename):
+def load_matrix_file(filename):
     with open(filename) as fd:
         text = fd.read()
     return parse_matrix(text)
 if __name__ == '__main__':
-    six.print_(load_matrix(sys.argv[1]))
+    six.print_(load_matrix_file(sys.argv[1]))
 # vim: set expandtab foldmethod=marker :
